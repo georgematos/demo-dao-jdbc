aaaf2c706501f4ac4b602ed7667fd82f0eaa1bde
George Matos <georgemattos@gmail.com>
2019-12-26 13:52:36 -0300

    delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -16,36 +16,30 @@ public class Program {
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 
 		System.out.println("------------find seller by id-------------");
-
 		Seller seller = sellerDao.findById(2);
-
 		System.out.println(seller);
 
 		System.out.println("------------sellers by department id-------------");
-
 		List<Seller> sellers = sellerDao.findByDepartment(new Department(1, null));
-
 		sellers.forEach(System.out::println);
 
 		System.out.println("------------all sellers-------------");
-
 		sellers = sellerDao.findAll();
-
 		sellers.forEach(System.out::println);
 
 		System.out.println("------------insert seller-------------");
-
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, new Department(2, null));
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted. New Id: " + newSeller.getId());
 
 		System.out.println("------------update seller-------------");
-
 		newSeller = sellerDao.findById(1);
 		newSeller.setBaseSalary(3500.0);
 		sellerDao.update(newSeller);
-
 		System.out.println("Updated Seller: " + newSeller);
+
+		System.out.println("------------delete seller-------------");
+		sellerDao.deleteById(13);
 
 		DB.closeConnection();
 
